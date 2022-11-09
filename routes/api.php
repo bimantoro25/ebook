530aa3fb64cec4f369d8dbab3fe860f23c6f1d4f
bimantoro25 <growtopia250704@gmail.com>
2022-11-09 06:33:45 -0800

    challenge author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,12 +21,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('me', [AuthController::class, 'me']);
-Route::get('/books', [BookController::class, 'index']);
-Route::get('/books/{id}', [BookController::class, 'swoh']);
-Route::post('/books', [BookController::class, 'store']); 
-Route::put('/books/{id}', [BookController::class,'update']);
-Route::delete('/books/{id}', [BookController::class,'destroy']);
+// Route::get('me', [AuthController::class, 'me']);
+// Route::get('/books', [BookController::class, 'index']);
+// Route::get('/books/{id}', [BookController::class, 'swoh']);
+// Route::post('/books', [BookController::class, 'store']); 
+// Route::put('/books/{id}', [BookController::class,'update']);
+// Route::delete('/books/{id}', [BookController::class,'destroy']);
+
+Route::get('/authors',[AuthorController::class, 'index']);
+Route::post('/authors',[AuthorController::class, 'store']);
+Route::get('/authors/{id}',[AuthorController::class, 'show']);
+Route::put('/authors/{id}',[AuthorController::class, 'update']);
+Route::delete('/authors/{id}',[AuthorController::class, 'destroy']);
 
 Route::resource('books', BookController::class)->except(
     ['create','edit']
